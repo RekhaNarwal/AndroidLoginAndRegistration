0ff5f95ac55f8e9eea2b623cd7b284b8a5bb31b6
RekhaNarwal <trustworthy788@gmail.com>
2015-03-23 12:13:19 +0530

    AndroidLoginAndRegistration
    Add Maps on the Project and also creating a  Activity on the App
    like as
    Profile, Post, Place, Friends and Setting

---
+++ app/src/main/java/com/socialpresencenetwork/phodo/RegisterActivity.java
@@ -0,0 +1,32 @@
+package com.socialpresencenetwork.phodo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set View to register.xml
+        setContentView(R.layout.register);
+
+        TextView loginScreen = (TextView) findViewById(R.id.btnRegister);
+
+        // Listening to Login Screen link
+        loginScreen.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                // Closing registration screen
+                // Switching to Login Screen/closing register screen
+               // finish();
+                Intent i = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(i);
+
+
+            }
+        });
+    }
+}
