48c2ccca0a444848b9aba46f11e0c45dcf2d607e
RekhaNarwal <trustworthy788@gmail.com>
2015-05-01 14:54:52 +0530

    add a new user on the resister page . and missing the work on map activity page i did not got the Zoom function on the mapactivity

--- app/src/main/java/com/socialpresencenetwork/phodo/FriendsActivity.java
+++ app/src/main/java/com/socialpresencenetwork/phodo/FriendsActivity.java
@@ -4,8 +4,10 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +20,12 @@ public class FriendsActivity extends Fragment {
       View v=inflater.inflate(R.layout.activity_friends,container,false);
         return v;
     }
-
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getActivity().getMenuInflater();
+       inflater.inflate(R.menu.menu_friends, menu);
+    }
 
 }
